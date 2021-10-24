656ac483ccba0cd12f62d4daa2d43a63127793a7
Luiz Dalla <40028036+luizdalla@users.noreply.github.com>
2021-10-24 10:16:27 -0300

    celery add

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bootstrapform',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_celery_results',
     
 ]
 
@@ -153,3 +154,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
